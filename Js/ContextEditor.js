833587fd194b51fa676b56768a7018f169a7868e
= <nathan.a.macleod@gmail.com>
2021-03-22 17:22:53 +0000

    Moved code into different files for organisational reasons

--- Js/ContextEditor.js
+++ Js/ContextEditor.js
@@ -1,33 +1,5 @@
 let mousePos = [0, 0];
 
-// This procedure removes everything in scene tree,
-// Then goes through all the objects in the THREE.js scene,
-// And adds a paragraph with the name of the object (making sure to highlight it if it's selected).
-function update_scene_tree(){
-    // Remove everything in the scene tree before adding stuff back in again.
-    sceneTree.replaceChildren();
-
-    // Add everything back in again.
-    function loop_through_scene(dictionary, indentLevel){
-        for (let key in dictionary){
-            if ((key == "children") || (!isNaN(key) == true)){
-                loop_through_scene(dictionary[key], indentLevel + 1);
-            } else if (key == "name"){
-                let htmlElement = add_element(sceneTree, "p");
-                htmlElement.innerHTML = dictionary[key];
-                htmlElement.style.marginLeft = indentLevel * 16 + "px";
-
-                if (dictionary[key] == selectedObject.name){
-                    htmlElement.style.color = "#00a1ff";
-                    htmlElement.style.fontWeight = "bold";
-                }
-            }
-        }
-    }
-
-    loop_through_scene(scene, 0);
-}
-
 // These function(s) allows you to select an object,
 // before updating the scene tree to highlight the object,
 // and updating the properties viewer to display the properties of the object that's just been selected.
@@ -35,22 +7,6 @@ sceneTree.addEventListener("click", function(e){
     let sceneObject = scene.getObjectByName(e.target.innerHTML);
     select_object(sceneObject);
 });
-
-function draw_bbox(object){
-    scene.remove(selectedObjectBBox);
-    selectedObjectBBox = new THREE.BoxHelper(object, 0xffff00);
-    scene.add(selectedObjectBBox);
-}
-
-function select_object(object){
-    if (object == undefined) return; 
-    
-    selectedObject = object;
-    draw_bbox(object);
-
-    update_scene_tree();
-    update_properties_editor();
-}
 
 // This function allows you to click on a button to add a new instance of it into the scene.
 objectCreator.addEventListener("click", function(e){
@@ -105,135 +61,6 @@ function update_transforms(axis){
     draw_bbox(selectedObject);
 };
 keyboardShortcuts.addEventListener("click", function(e){ update_transforms(e.target.innerHTML) });
-
-// Function(s) to add input fields to update attributes of the selected object.
-let focusing = false; // Keep track of whether the user is clicking on an input field
-
-// This function automates some stuff to do with the properties editor.
-// It adds a label & input field, handles some stuff to do with focusing, etc.
-function add_input_field(htmlElement, title, inputType, value){
-    add_element(htmlElement, "p").innerHTML = title;
-
-    var inputField = add_element(htmlElement, "input");
-    inputField.type = inputType;
-    inputField.value = value;
-    inputField.addEventListener("focus", function(){ focusing = true });
-    inputField.addEventListener("focusout", function(){ focusing = false });
-
-    return inputField;
-}
-
-function update_properties_editor(){
-    // First, remove everything in "propertyEditor" (before adding an updated version of it back again)
-    propertyEditor.replaceChildren();
-
-    // Next, add a button to delete the selected object
-    let deleteObjectBtn = add_element(propertyEditor, "button");
-    deleteObjectBtn.innerHTML = "Delete This Object";
-    deleteObjectBtn.addEventListener("click", function(){
-        if (confirm("Are you sure you want to delete '" + selectedObject.name + "'?")){
-            scene.remove(selectedObject);
-            selectedObject = scene;
-            update_scene_tree();
-        }
-    });
-
-    add_element(propertyEditor, "hr");
-
-    // Then, add input fields for the object if they exist (colour, position, etc)
-    for (let key in selectedObject){
-        // Avoid looping through everything twice (because of __proto__ objects)
-        if (selectedObject.hasOwnProperty(key)){
-            if (key == "name"){
-                let nameInput = add_input_field(propertyEditor, "Name: ", "text", selectedObject.name);
-                nameInput.addEventListener("change", function(){
-                    selectedObject.name = nameInput.value;
-                    update_scene_tree();
-                });
-
-                add_element(propertyEditor, "hr");
-            } else if (key == "color"){
-                let colorR = selectedObject.color.r * 255;
-                let colorG = selectedObject.color.g * 255;
-                let colorB = selectedObject.color.b * 255;
-
-                let colorInput = add_input_field(propertyEditor, "Color: ", "color", rgbToHex(colorR, colorG, colorB));
-                colorInput.addEventListener("change", function(){
-                    selectedObject.color.r = hexToRgb(colorInput.value).r / 255;
-                    selectedObject.color.g = hexToRgb(colorInput.value).g / 255;
-                    selectedObject.color.b = hexToRgb(colorInput.value).b / 255;
-                });
-
-                add_element(propertyEditor, "hr");
-            } else if (key == "material"){
-                let colorR = selectedObject.material.color.r * 255;
-                let colorG = selectedObject.material.color.g * 255;
-                let colorB = selectedObject.material.color.b * 255;
-
-                let materialColorInput = add_input_field(propertyEditor, "Material Color: ", "color", rgbToHex(colorR, colorG, colorB));
-                materialColorInput.addEventListener("change", function(){
-                    selectedObject.material.color.r = hexToRgb(materialColorInput.value).r / 255;
-                    selectedObject.material.color.g = hexToRgb(materialColorInput.value).g / 255;
-                    selectedObject.material.color.b = hexToRgb(materialColorInput.value).b / 255;
-                });
-
-                add_element(propertyEditor, "hr");
-            } else if (key == "position"){
-                let positionInputX = add_input_field(propertyEditor, "Position X: ", "number", selectedObject.position.x);
-                let positionInputY = add_input_field(propertyEditor, "Position Y: ", "number", selectedObject.position.y);
-                let positionInputZ = add_input_field(propertyEditor, "Position Z: ", "number", selectedObject.position.z);
-                positionInputX.step = 0.25;
-                positionInputY.step = 0.25;
-                positionInputZ.step = 0.25;
-                positionInputX.addEventListener("change", function(){
-                    selectedObject.position.x = positionInputX.value;
-                });
-                positionInputY.addEventListener("change", function(){
-                    selectedObject.position.y = positionInputY.value;
-                });
-                positionInputZ.addEventListener("change", function(){
-                    selectedObject.position.z = positionInputZ.value;
-                });
-
-                add_element(propertyEditor, "hr");
-            } else if (key == "rotation"){
-                let rotationInputX = add_input_field(propertyEditor, "Rotation X: ", "number", THREE.Math.radToDeg(selectedObject.rotation.x));
-                let rotationInputY = add_input_field(propertyEditor, "Rotation Y: ", "number", THREE.Math.radToDeg(selectedObject.rotation.y));
-                let rotationInputZ = add_input_field(propertyEditor, "Rotation Z: ", "number", THREE.Math.radToDeg(selectedObject.rotation.z));
-                rotationInputX.step = 0.25;
-                rotationInputY.step = 0.25;
-                rotationInputZ.step = 0.25;
-                rotationInputX.addEventListener("change", function(){
-                    selectedObject.rotation.x = THREE.Math.degToRad(rotationInputX.value);
-                });
-                rotationInputY.addEventListener("change", function(){
-                    selectedObject.rotation.y = THREE.Math.degToRad(rotationInputY.value);
-                });
-                rotationInputZ.addEventListener("change", function(){
-                    selectedObject.rotation.z = THREE.Math.degToRad(rotationInputZ.value);
-                });
-
-                add_element(propertyEditor, "hr");
-            } else if (key == "scale"){
-                let scaleInputX = add_input_field(propertyEditor, "Scale X: ", "number", selectedObject.scale.x)
-                let scaleInputY = add_input_field(propertyEditor, "Scale Y: ", "number", selectedObject.scale.y)
-                let scaleInputZ = add_input_field(propertyEditor, "Scale Z: ", "number", selectedObject.scale.z)
-                scaleInputX.step = 0.25;
-                scaleInputY.step = 0.25;
-                scaleInputZ.step = 0.25;
-                scaleInputX.addEventListener("change", function(){
-                    selectedObject.scale.x = scaleInputX.value;
-                });
-                scaleInputY.addEventListener("change", function(){
-                    selectedObject.scale.y = scaleInputY.value;
-                });
-                scaleInputZ.addEventListener("change", function(){
-                    selectedObject.scale.z = scaleInputZ.value;
-                });
-            }
-        }
-    }
-}
 
 // This procedure opens the context editor, and places it in the middle of the mouse.
 function show_context_editor(){
