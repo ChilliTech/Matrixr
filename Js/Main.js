bb98018c186e50004420075095f9258d20af82a1
= <nathan.a.macleod@gmail.com>
2021-03-25 21:36:28 +0000

    Updated the css, added a folder containing all the stylesheets

---
+++ Js/Main.js
@@ -0,0 +1,16 @@
+// This function runs each frame and renders the scene
+function animate() {
+    // Handle the window resizing
+    window.addEventListener("resize", () => {
+        canvas.style.width = "100%";
+        canvas.style.height = "100%";
+        camera.aspect = canvas.offsetWidth / canvas.offsetHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(canvas.offsetWidth, canvas.offsetHeight);
+    });
+
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+
+animate();
--- Js/Scene.js
+++ Js/Scene.js
@@ -11,3 +11,58 @@ function is_focused(){
 }
 
 is_focused();
+
+// Setup the three.js scene
+let canvas = document.getElementById("mainCanvas");
+
+let scene = new THREE.Scene();
+scene.background = new THREE.Color(0xf8f8f8);
+scene.name = "MyFirstProject";
+
+// The camera
+let camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.y = 5;
+camera.position.z = 10;
+camera.lookAt(0, 0, 0);
+
+// The renderer
+let renderer = new THREE.WebGLRenderer(
+    {
+        alpha: true,
+        preserveDrawingBuffer: true,
+        antialias: true,
+        canvas: canvas
+    }
+);
+
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.gammaFactor = 0.2;
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.physicallyCorrectLights;
+camera.aspect = canvas.offsetWidth / canvas.offsetHeight;
+camera.updateProjectionMatrix();
+renderer.setSize(canvas.offsetWidth, canvas.offsetHeight);
+document.body.appendChild(renderer.domElement);
+
+// The orbit controls
+let controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+// The directional light & helper
+let directionalLight = new THREE.DirectionalLight(0xcccccc);
+directionalLight.position.set(1.5, 2, 6);
+directionalLight.name = "Directional Light";
+scene.add(directionalLight);
+
+let directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 2, 0x888888);
+directionalLightHelper.name = "Directional Light Helper";
+directionalLight.add(directionalLightHelper);
+
+// The ambient light
+let ambientLight = new THREE.AmbientLight(scene.background, 0.15);
+ambientLight.name = "Ambient Light";
+scene.add(ambientLight);
+
+// The grid floor
+let gridFloor = new THREE.GridHelper(12, 12, 0x888888, 0x888888);
+gridFloor.name = "Grid Floor";
+scene.add(gridFloor);
